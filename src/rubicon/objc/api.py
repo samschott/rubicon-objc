077fd49d128fb9debad69c395222b0ee39afa344
samschott <sam.schott@outlook.com>
2024-11-26 21:34:22 +0100

    black code formatting

--- src/rubicon/objc/api.py
+++ src/rubicon/objc/api.py
@@ -809,6 +809,7 @@ class ObjCInstance:
     #
     # Refs #251.
     _instance_lock = threading.RLock()
+
     @property
     def objc_class(self):
         """The Objective-C object's class, as an :class:`ObjCClass`."""
